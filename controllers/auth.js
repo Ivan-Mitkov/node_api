2935c145cae092dd07faef45c8563f396e226451
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-02-18 08:36:51 +0200

    reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 const sendEmail = require("../utils/sendEmail.js");
@@ -71,7 +72,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   //Create reset url
   const resetUrl = `${req.protocol}://${req.get(
     "host"
-  )}/api/v1/resetpassword/${resetToken}`;
+  )}/api/v1/auth/resetpassword/${resetToken}`;
 
   //Message
   const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
@@ -87,10 +88,39 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     //remove frm DB resetPasswordToken and expires
     user.resetPasswordToken = undefined;
     user.resetPasswordExpire = undefined;
-    await user.save({validateBeforeSave:false});
-    return next(new ErrorResponse("Email could not be send", 500))
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorResponse("Email could not be send", 500));
   }
   // res.status(200).json({ success: true, data: user });
+});
+
+//@desk Reset password
+//@route Put /api/v1/auth/resetpassword/:resettoken
+//@access Private
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  //Get hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resettoken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  });
+  if (!user) {
+    next(new ErrorResponse("Invalid token", 400));
+  }
+  //Set new password
+  user.password = req.body.password;
+  //null reset property
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  //save user
+  await user.save();
+
+  sentTokenResponse(user, 200, res);
 });
 
 //Get token from model, create cookie, send response
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/me", protect, authController.getMe);
 router.post("/forgotpassword", authController.forgotPassword);
+router.put("/resetpassword/:resettoken", authController.resetPassword);
 
 module.exports = router;
