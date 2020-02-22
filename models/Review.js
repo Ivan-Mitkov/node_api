6178876b632b23a093a63f3ec68ad94eb1daab56
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-02-22 15:14:10 +0200

    aggregation average rating

--- models/Review.js
+++ models/Review.js
@@ -35,6 +35,46 @@ const ReviewSchema = new mongoose.Schema({
 });
 
 //Prevent user from submitting more than one review per bootcamp
+//https://docs.mongodb.com/manual/core/index-compound/
+// https://docs.mongodb.com/manual/core/index-unique/
+//Must create index in the Atlas to work
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+//Aggregation static method to get average rating
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+  // check if working
+  console.log("Chek average pre and post".blue);
+  const obj = await this.aggregate([
+    {
+      $match: {
+        bootcamp: bootcampId
+      }
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageRating: {
+          $avg: "$rating"
+        }
+      }
+    }
+  ]);
+  console.log(obj);
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    });
+  } catch (error) {
+    console.log(err);
+  }
+};
+
+//Aggregation call getAverageRating after save
+ReviewSchema.post("save", function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+//Aggregation call getAverageRating before delete
+ReviewSchema.pre("remove", function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
 module.exports = mongoose.model("Review", ReviewSchema);
